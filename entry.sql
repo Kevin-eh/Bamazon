339e941a18ad1dc838523f1a50fd4217ea1e4f1a
Kevin-eh <54459190+Kevin-eh@users.noreply.github.com>
2019-12-14 19:25:35 -0800

    some SQL code thrown in for good measure

---
+++ entry.sql
@@ -0,0 +1,11 @@
+drop database if exists bamazon;
+create database bamazon;
+use bamazon;
+CREATE TABLE products(
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price INT default 0,
+  stock_quantity INT default 0,
+  PRIMARY KEY (item_id)
+);
